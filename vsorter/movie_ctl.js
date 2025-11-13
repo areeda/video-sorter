d63430a745d47fe436058f94c2f98b9f483761f2
Joseph Areeda <joseph.areeda@ligo.org>
2025-11-13 11:42:02 -0800

    fixx play after movie finished

--- vsorter/movie_ctl.js
+++ vsorter/movie_ctl.js
@@ -2,6 +2,7 @@ function movie_start(movie_id, speed_label_id, speed)
 {
     movie = $jQuery('#id');
     movie.playbackRate = speed;
+    movie.pause();
     movie.play();
     var speed_label = $jQuery('#speed_label');
     speed_label.innerHTML = speed.toFixed(2);
